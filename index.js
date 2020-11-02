c9cf4911a94f0f62b5424e346cffe441342c2567
victorakesson <65647015+victorakesson@users.noreply.github.com>
2020-11-02 14:31:42 +0100

    del 2

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function randomInt() {
-return [ 
-    Math.floor(Math.random() * 100),
+return  Math.floor(Math.random() * 100)
 
-]
-    
 }
 
-console.log(randomInt())
+const secretNumber = randomInt()
+
+console.log(secretNumber)
+
+function getUserGuess() {
+const gissning = document.getElementById("user-input").value
+return parseInt(gissning, 10)
+
+    
+}
