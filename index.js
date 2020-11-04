6a3ac68e02531af7540e7f4e12a8c92c331cab81
victorakesson <65647015+victorakesson@users.noreply.github.com>
2020-11-04 13:44:37 +0100

    del 4 och 5

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 function randomInt(n) {
-return  Math.floor(Math.random() * n)
-
+   return  Math.floor(Math.random() * n) + 1 
 }
 
 const secretNumber = randomInt(100)
-
 console.log(secretNumber)
 
-function getUserGuess() {
-const guess = document.getElementById("user-input").value
-return parseInt(guess, 10) 
+function getUserGuess(){
+     const guess = document.getElementById("user-input").value
+     return parseInt(guess, 10) 
 }
 
 
 
-document.addEventListener("keyup", function (tryck) {
-if (tryck.key === "enter") {
-const number = Getnummer()
-console.log(number)
-}
-})
-
-function setMessage(msg) {
-const txt = document.getElementById('message').innerText = "Fel"
-if 
+document.addEventListener("keyup", function (event) {
+    if (event.key === "enter") {
+        if (randomNumber === getUserGuess()){
+            setMessage("Well Done!")
+        } else if (randomNumber > getUserGuess()) {
+            setMessage("Too low")
+        } else if (randomNumber < getUserGuess()) {
+            setMessage("Too high")
+        }
+         document.getElementById("user-input").value = " "
     
+}
+
+function setMessage(msg){
+    document.getElementById("message").innerText = msg
 }
 
